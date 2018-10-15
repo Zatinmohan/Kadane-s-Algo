b801d5156e47c51073316d647faa5c0f7a22c62a
Zatinmohan <37806553+Zatinmohan@users.noreply.github.com>
2018-10-16 00:55:15 +0530

    Create Kadane's Algo.cpp

---
+++ Kadane's Algo.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * example - 2 -3 4 -1 -2 1 5 -3
+   output  - 7 
+   how -  4+ (-1) + (-2) + 1 + 5 = 7.
+*/
+int main()
+{
+  int size,max_sum=INT_MIN,sum=0;
+  
+  cin>>size;							//size of array
+  
+  int arr[size];						//array inilisation
+  
+  for(int i=0;i<size;i++)
+  {
+  	cin>>arr[i];						//input in array
+  }
+  
+  for(int i=0;i<size;i++)
+  {
+  	sum+=arr[i];						//finding out the sum.
+	
+	if(max_sum<sum)						//if sum is greater then max sum than max sum will become the sum.
+		max_sum=sum;
+	if(sum<0)							//if sum is negative then make sum 0 and continue with the loop.
+		sum=0;
+  }
+  
+  cout<<"SUM : "<<max_sum;
+  return 0;
+}
